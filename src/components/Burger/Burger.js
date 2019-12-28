86fb4e04eb7cf10a35c2a8d09f144d73b9fcc521
rahul reddy <42753261+rahulreddy062@users.noreply.github.com>
2019-12-28 17:08:53 -0600

    Add functionalites to build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,6 +4,7 @@ import classes from './Burger.css';
 const burger = (props)=>{
     let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey=>{
+        
         return [...Array(props.ingredients[igKey])].map((_,i)=>{
            return  <BurgerIngredient key={igKey + i} type={igKey} />
         })
